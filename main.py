c8a2dfc81d54f3382176abd2329448c4bad54730
drdepe <drdepe7@gmail.com>
2022-01-11 10:54:50 +0200

    sc

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import constants as keys
 from telegram.ext import *
 import responses as r 
 
-print("Bot started")
+print("Bot is running ")
 
 def start_command(update, context):
     update.message.reply_text("Say something .")
@@ -20,8 +20,10 @@ def helpme(update , context):
 
 ⭕️ Commands:-
 
-/calc1 : شيتات الحسبان سمستر 1
-/stats1 : شيتات الاحصاء سمستر 1
+/calc1  : شيتات الحسبان سمستر 1
+/stats1  : شيتات الاحصاء سمستر 1
+/linear1 : شيتات الجبر سمستر 1
+/cs1     : شيتات الحاسوب سمستر 1
     """
     update.message.reply_text(msg)
 
@@ -29,6 +31,10 @@ def handle_message(update, context):
     text = update.message.text
     response = r.res(text)
     update.message.reply_text(response)
+def error(update , context):
+    print(f"Update {update} caused error {context.error}")
+
+# ///////////////////////////////////////////////////////////////////////////
 
 def calc1(update, context):
   update.message.reply_text("جاري ارسال شيتات الحسبان 🗂")
@@ -43,9 +49,14 @@ def stats1(update, context):
     update.message.reply_text("جاري ارسال شيتات الاحصاء 🗂")
     for i in range(1,10):
      context.bot.sendDocument(update.effective_chat.id , document=open(f"./sheets/stats1/Statistics_lecture{i}.pdf" , 'rb'))
-def error(update , context):
-    print(f"Update {update} caused error {context.error}") 
+def cs1(update, context):
+    update.message.reply_text("جاري ارسال شيتات الحاسوب 🗂")
+    for i in range(1,11):
+     context.bot.sendDocument(update.effective_chat.id , document=open(f"./sheets/cs1/{i}.pdf" , 'rb'))
 
+ 
+
+# ////////////////////////////
 def main():
     updater = Updater(keys.api_key, use_context=True)
     dp = updater.dispatcher
@@ -54,6 +65,7 @@ def main():
     dp.add_handler(CommandHandler("calc1", calc1))
     dp.add_handler(CommandHandler("stats1", stats1))
     dp.add_handler(CommandHandler("linear1", linear1))
+    dp.add_handler(CommandHandler("cs1", cs1))
     dp.add_handler(MessageHandler(Filters.text, handle_message))
     dp.add_error_handler(error)
     updater.start_polling()
